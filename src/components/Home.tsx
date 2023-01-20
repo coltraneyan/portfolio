54fe37a2af9f022cd1f8eb9535e9444f6153cbc9
COLTRANE YAN <coltraneyan@gmail.com>
2023-01-19 20:57:07 -0500

    update

---
+++ src/components/Home.tsx
@@ -0,0 +1,41 @@
+
+export const Home = () => {
+  return (
+    <div className="stack">
+      <div className="header">coltrane.cc</div>
+      <div>
+        i'm a front-end engineer working on defi web apps
+      </div>
+      <div className="subheader">production apps</div>
+      <a href="https://app.rook.fi" target="_blank" rel="noreferrer">
+        rook trade
+      </a>
+      <a href="https://airswap.io/#make" target="_blank" rel="noreferrer">
+        airswap otc
+      </a>
+      <div className="subheader">research</div>
+      <a
+        href="https://github.com/coltraneyan/bigdatacupyan/blob/main/Big%20Data%20Cup%20-%20Pass%20Value%20Added.pdf"
+        target="_blank"
+        rel="noreferrer"
+      >
+        pass value added
+      </a>
+      <div className="subheader">info</div>
+      <a
+        href="https://github.com/coltraneyan"
+        target="_blank"
+        rel="noreferrer"
+      >
+        github
+      </a>
+      <a
+        href="mailto:coltraneyan@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        email
+      </a>
+    </div>
+  );
+};
--- src/components/functions/toggleActive.tsx
+++
@@ -1,7 +0,0 @@
-function toggleActive(target: HTMLDivElement, tag: string): void {
-  target.classList.contains(tag)
-    ? target.classList.remove(tag)
-    : target.classList.add(tag);
-}
-
-export default toggleActive;
